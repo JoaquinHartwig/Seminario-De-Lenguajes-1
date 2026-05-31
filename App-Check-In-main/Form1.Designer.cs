cd012b07dbba5d6f67c8e8e782cd2af59395d1e1
JoaquinHartwig <yanelian29@gmail.com>
2026-05-31 04:01:58 -0300

    Limpio archivos innecesarios

---
+++ App-Check-In-main/Form1.Designer.cs
@@ -0,0 +1,359 @@
+﻿namespace App_Check_In
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.txtApellido = new System.Windows.Forms.TextBox();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.textDocumento = new System.Windows.Forms.MaskedTextBox();
+            this.txtPasajero = new System.Windows.Forms.MaskedTextBox();
+            this.rdNacional = new System.Windows.Forms.RadioButton();
+            this.rdInternacional = new System.Windows.Forms.RadioButton();
+            this.cbxTarifa = new System.Windows.Forms.ComboBox();
+            this.btnProcesar = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label9 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Tai Le", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(54, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(497, 23);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Aplicacion para Check -in / Aerolineas Argentinas 2026";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Tai Le", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(54, 96);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(235, 21);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Ingrese los datos del pasajero";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(60, 132);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(58, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Apellido(s):";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(57, 168);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(58, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Nombre(s):";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(60, 200);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(82, 13);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Nro.Documento";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(57, 230);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(119, 13);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "Nro.Pasajero Frecuente";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(60, 263);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(72, 13);
+            this.label7.TabIndex = 7;
+            this.label7.Text = "Tipo de vuelo";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(63, 300);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(69, 13);
+            this.label8.TabIndex = 8;
+            this.label8.Text = "Tipo de tarifa";
+            // 
+            // txtApellido
+            // 
+            this.txtApellido.Location = new System.Drawing.Point(182, 129);
+            this.txtApellido.Name = "txtApellido";
+            this.txtApellido.Size = new System.Drawing.Size(100, 20);
+            this.txtApellido.TabIndex = 9;
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(182, 165);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(100, 20);
+            this.txtNombre.TabIndex = 10;
+            // 
+            // textDocumento
+            // 
+            this.textDocumento.Location = new System.Drawing.Point(182, 200);
+            this.textDocumento.Mask = "##,###,###";
+            this.textDocumento.Name = "textDocumento";
+            this.textDocumento.Size = new System.Drawing.Size(62, 20);
+            this.textDocumento.TabIndex = 11;
+            // 
+            // txtPasajero
+            // 
+            this.txtPasajero.Location = new System.Drawing.Point(182, 230);
+            this.txtPasajero.Name = "txtPasajero";
+            this.txtPasajero.Size = new System.Drawing.Size(100, 20);
+            this.txtPasajero.TabIndex = 12;
+            // 
+            // rdNacional
+            // 
+            this.rdNacional.AutoSize = true;
+            this.rdNacional.Location = new System.Drawing.Point(153, 263);
+            this.rdNacional.Name = "rdNacional";
+            this.rdNacional.Size = new System.Drawing.Size(67, 17);
+            this.rdNacional.TabIndex = 14;
+            this.rdNacional.TabStop = true;
+            this.rdNacional.Text = "Nacional";
+            this.rdNacional.UseVisualStyleBackColor = true;
+            this.rdNacional.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // rdInternacional
+            // 
+            this.rdInternacional.AutoSize = true;
+            this.rdInternacional.Location = new System.Drawing.Point(244, 263);
+            this.rdInternacional.Name = "rdInternacional";
+            this.rdInternacional.Size = new System.Drawing.Size(86, 17);
+            this.rdInternacional.TabIndex = 15;
+            this.rdInternacional.TabStop = true;
+            this.rdInternacional.Text = "Internacional";
+            this.rdInternacional.UseVisualStyleBackColor = true;
+            // 
+            // cbxTarifa
+            // 
+            this.cbxTarifa.FormattingEnabled = true;
+            this.cbxTarifa.Items.AddRange(new object[] {
+            "P",
+            "B",
+            "F",
+            "PE"});
+            this.cbxTarifa.Location = new System.Drawing.Point(161, 297);
+            this.cbxTarifa.Name = "cbxTarifa";
+            this.cbxTarifa.Size = new System.Drawing.Size(121, 21);
+            this.cbxTarifa.TabIndex = 16;
+            this.cbxTarifa.SelectedIndexChanged += new System.EventHandler(this.cbxTarifa_SelectedIndexChanged);
+            // 
+            // btnProcesar
+            // 
+            this.btnProcesar.Location = new System.Drawing.Point(51, 370);
+            this.btnProcesar.Name = "btnProcesar";
+            this.btnProcesar.Size = new System.Drawing.Size(169, 30);
+            this.btnProcesar.TabIndex = 17;
+            this.btnProcesar.Text = "Procesar";
+            this.btnProcesar.UseVisualStyleBackColor = true;
+            this.btnProcesar.Click += new System.EventHandler(this.btnProcesar_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(149, 188);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(169, 30);
+            this.button1.TabIndex = 18;
+            this.button1.Text = "Pruebas ";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Tai Le", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(15, 34);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(221, 21);
+            this.label9.TabIndex = 20;
+            this.label9.Text = "Escriba el peso del equipaje";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(19, 58);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 21;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(19, 90);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 22;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(19, 127);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 23;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(182, 113);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(136, 47);
+            this.button2.TabIndex = 24;
+            this.button2.Text = "Reiniciar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(258, 370);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(126, 38);
+            this.button3.TabIndex = 25;
+            this.button3.Text = "Verificar equipaje";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.LightCoral;
+            this.groupBox1.Controls.Add(this.label10);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.textBox3);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.label9);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Location = new System.Drawing.Point(386, 103);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(331, 283);
+            this.groupBox1.TabIndex = 26;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Segoe UI Historic", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(15, 235);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(129, 20);
+            this.label10.TabIndex = 25;
+            this.label10.Text = "INTERFAZ VIEJA";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(717, 450);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.btnProcesar);
+            this.Controls.Add(this.cbxTarifa);
+            this.Controls.Add(this.rdInternacional);
+            this.Controls.Add(this.rdNacional);
+            this.Controls.Add(this.txtPasajero);
+            this.Controls.Add(this.textDocumento);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.txtApellido);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Aerolineas Argentinas 2026";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox txtApellido;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.MaskedTextBox textDocumento;
+        private System.Windows.Forms.MaskedTextBox txtPasajero;
+        private System.Windows.Forms.RadioButton rdNacional;
+        private System.Windows.Forms.RadioButton rdInternacional;
+        private System.Windows.Forms.ComboBox cbxTarifa;
+        private System.Windows.Forms.Button btnProcesar;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label10;
+    }
+}
+
---
+++ App-Check-In-main/Modelos/Pasajeros.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App_Check_In.Modelos
+{
+    public class Pasajeros
+    {
+        private string _nombre;
+        private string _apellido;
+        private string _dni;
+        private string _pasajerosfrecuentes;
+        private List<Equipaje> _equipajes = new List<Equipaje>();
+
+        public Pasajeros(string nombre, string apellido, string dni, string pasajerosfrecuentes) // constrvuctor
+        {
+            _nombre = nombre;
+            _apellido = apellido;
+            _dni = dni;
+            _pasajerosfrecuentes = pasajerosfrecuentes;
+
+        }
+
+
+        public string Nombre
+        {
+            get { return _nombre; }
+            set
+            {
+                if (value != string.Empty)
+                {
+
+                    _nombre = value;
+                }
+            }
+        }
+        public string Apellido
+        {
+            get { return _apellido; }
+            set
+            {
+                if (value != string.Empty)
+                {
+                    _apellido = value;
+                }
+            }
+        }
+        public string Dni
+        {
+            get { return _dni; }
+            set { _dni = value; }
+            {
+                if (value != string.Empty)
+                {
+                    _dni = value;
+                }
+            }
+        }
+        public string Pasajerosfrecuentes
+        {
+            get { return _pasajerosfrecuentes; }
+            set
+            {
+                if (value != string.Empty)
+                {
+                    _pasajerosfrecuentes = value;
+                }
+            }
+       
+        
+        }
+         public List<Equipaje> GetEquipajes()
+            { return _equipajes; }
+
+
+        public void AgregarEquipaje(Equipaje equipaje)
+        {
+            _equipajes.Add(equipaje);
+
+    }
+
+    }
+} 
+
+ 
