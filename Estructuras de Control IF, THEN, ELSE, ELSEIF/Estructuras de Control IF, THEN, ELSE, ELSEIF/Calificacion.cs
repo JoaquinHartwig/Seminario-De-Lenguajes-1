dce406dfb4893b89c5f062565ffbf24597d096cf
JoaquinHartwig <yanelian29@gmail.com>
2026-05-31 03:45:48 -0300

    Initial commit

---
+++ Estructuras de Control IF, THEN, ELSE, ELSEIF/Estructuras de Control IF, THEN, ELSE, ELSEIF/Calificacion.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Estructuras_de_Control_IF__THEN__ELSE__ELSEIF
+{
+    public partial class Calificacion : Form
+    {
+        public Calificacion()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int resultado = Convert.ToInt32(textBox1.Text);
+            string calificacion = "";
+            if (resultado >= 90 && resultado<= 100)
+                calificacion = "A";
+            else if (resultado >= 80 && resultado <= 89)
+                calificacion = "B";
+            else if (resultado >= 70 && resultado <= 79)
+                calificacion = "C";
+            else if (resultado >= 60 && resultado <= 69)
+                calificacion = "D";
+            else if (resultado >= 40 && resultado <= 59)
+                calificacion = "E";
+            else
+                calificacion = "F";
+
+          MessageBox.Show($" La Calificación es: {calificacion}");
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Estructuras de Control IF, THEN, ELSE, ELSEIF/Estructuras de Control IF, THEN, ELSE, ELSEIF/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Estructuras_de_Control_IF__THEN__ELSE__ELSEIF
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Calificacion calificacion = new Calificacion();
+            calificacion.Show();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            LeerNúmeros leerNúmeros = new LeerNúmeros();
+            leerNúmeros.Show();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Leer3Numeroscs leer3Numeroscs = new Leer3Numeroscs();
+            leer3Numeroscs.Show();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Fechas fechas = new Fechas();
+            fechas.Show();
+        }
+    }
+}
